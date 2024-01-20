32fb2f41cfd04330dc13a46a8068a2f7365d405c
hainh1712 <nguyenhoanghai20021712@gmail.com>
2024-01-20 15:48:55 +0700

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,21 @@
-
+import { Input, Space } from 'antd';
 const Header = () => {
+  const { Search } = Input;
+  const handleSearch = (value) => {
+    console.log(value)
+  };
+
   return (
-    <div className="bg-[#fdcf76] h-10">
-      Login
+    <div className="bg-[#fdcf76] h-16 flex">
+      <div className='flex w-3/5 mx-auto items-center'>
+        <div className='pl-10 pr-20'>
+          <img src="/src/assets/cat.jpg" alt="" width={40} height={40} />
+        </div>
+        <div>
+          <Search placeholder="Search manga" allowClear style={{ width: 600 }} onSearch={handleSearch}/>
+        </div>
+      </div>
+      
     </div>
   );
 }
