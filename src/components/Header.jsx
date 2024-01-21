5af47772e32e4c8484d73b787a814e22d79f6828
hainh1712 <nguyenhoanghai20021712@gmail.com>
2024-01-21 11:01:10 +0700

    update yugioh

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,12 @@ const Header = () => {
   };
 
   return (
-    <div className="bg-[#fdcf76] h-16 flex sticky top-0">
+    <div className="bg-[#fdcf76] h-16 flex sticky top-0 z-10">
       <div className='flex w-3/5 mx-auto items-center'>
         <div className='pl-10 pr-20'>
-          <img src="/cat.jpg" alt="" width={40} height={40} />
+          <a href="/">
+            <img src="/cat.jpg" alt="" width={40} height={40} />
+          </a>
         </div>
         <div>
           <Search placeholder="Search manga" allowClear style={{ width: 600 }} onSearch={handleSearch}/>
